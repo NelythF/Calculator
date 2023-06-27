0542bcbde9355582e1840548b3151573d3ce48b5
NelythF <nelythfaria@hotmail.com>
2023-06-28 07:47:58 +0800

    calculator v0.02
    *Fully styled calculator
    *started with functionality of calculator

---
+++ calculator.js
@@ -0,0 +1,42 @@
+document.getElementById("one") = function(){clickOne()};
+document.getElementById("two") = function(){clickTwo()};
+document.getElementById("three") = function(){clickThree()};
+document.getElementById("four") = function(){clickFour()};
+document.getElementById("five") = function(){clickFive()};
+document.getElementById("six") = function(){clickSix()};
+document.getElementById("seven") = function(){clickSeven()};
+document.getElementById("eight") = function(){clickEight()};
+document.getElementById("nine") = function(){clickNine()};
+document.getElementById("zero") = function(){clickZero()};
+document.getElementById("decimal") = function(){clickDecimal()};
+document.getElementById("ac") = function(){clickAC()};
+document.getElementById("divide") = function(){clickDivide()};
+document.getElementById("multiply") = function(){clickMultiply()};
+document.getElementById("add") = function(){clickAdd()};
+document.getElementById("subtract") = function(){clickSubtract()};
+document.getElementById("equals") = function(){clickEquals()}
+
+clickAC() {
+
+}
+
+clickAdd() {
+
+}
+
+clickDivide() {
+
+}
+
+clickSubtract() {
+
+}
+
+clickMultiply() {
+
+}
+
+clickEquals() {
+
+}
+
